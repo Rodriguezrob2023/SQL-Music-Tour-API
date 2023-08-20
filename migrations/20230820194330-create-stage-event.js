3c3c1a0c2e13262916175f97a2f834b62c299cfa
Roberto Rodriguez <jrhitman5150@gmail.com>
2023-08-20 12:47:19 -0700

    Update 20230820194330-create-stage-event.js

--- migrations/20230820194330-create-stage-event.js
+++ migrations/20230820194330-create-stage-event.js
@@ -2,33 +2,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('StageEvents', {
-      id: {
+    await queryInterface.createTable('stage_events', {
+      stage_event_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      stage_event_id: {
-        type: Sequelize.INTEGER
-      },
       event_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       stage_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('StageEvents');
+    await queryInterface.dropTable('stage_events');
   }
 };
